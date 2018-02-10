97de8368e117778096b9fe3f78f7832ef7a619ec
João Pedro Perondini <jpfperondini@gmail.com>
2018-02-10 21:54:43 -0200

    Collision improvements

--- collision.lua
+++ collision.lua
@@ -3,11 +3,13 @@ Collision = {}
 function Collision:check(ballPos, ballSpeed, paddle1Pos, paddle2Pos)
     if ballPos.x <= (paddle1Pos.x + PADDLE_WIDTH) and
             ballPos.y >= paddle1Pos.y and
-            ballPos.y <= (paddle1Pos.y + PADDLE_HEIGHT - BALL_SIZE) then
+            ballPos.y <= (paddle1Pos.y + PADDLE_HEIGHT - BALL_SIZE) and
+            ballSpeed.x < 0 then
         ballSpeed.x = -ballSpeed.x
     elseif ballPos.x >= (paddle2Pos.x - PADDLE_WIDTH) and
             ballPos.y >= paddle2Pos.y and
-            ballPos.y <= (paddle2Pos.y + PADDLE_HEIGHT - BALL_SIZE) then
+            ballPos.y <= (paddle2Pos.y + PADDLE_HEIGHT - BALL_SIZE) and
+            ballSpeed.x > 0 then
         ballSpeed.x = -ballSpeed.x
     end
 end
